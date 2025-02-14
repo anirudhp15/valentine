90472f7ea5f3b7f4bed11d4e284730cff1d22c99
Ani Pottammal <ap7564@nyu.edu>
2025-02-13 20:22:57 -0500

    made pages folder with 3 routes

---
+++ src/pages/valentines.jsx
@@ -0,0 +1,12 @@
+export default function ValentinesPage() {
+  return (
+    <div>
+      <h1>Happy Valentine’s Day, Pooja!</h1>
+      <p style={{ whiteSpace: "pre-line" }}>
+        {/* Placeholder for your letter */}
+        [Your letter to Pooja goes here. You can copy and paste text, even line
+        breaks will be preserved if you use 'pre-line'.]
+      </p>
+    </div>
+  );
+}
