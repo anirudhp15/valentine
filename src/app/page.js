c99f227a137c6a12c53260c1e3cfc01568649f48
Ani Pottammal <ap7564@nyu.edu>
2025-02-13 20:07:14 -0500

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid">
-      <h1 className="text-4xl font-bold text-center">Hello, World!</h1>
+    <div className="">
+      <h1 className="">Hello, World!</h1>
     </div>
   );
 }
